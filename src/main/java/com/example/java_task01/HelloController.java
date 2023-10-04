594b3ff3a63349083ba5b628f83c4e9a54f6a83f
Anion11 <nik.klyukoit@bk.ru>
2023-10-04 20:31:10 +0300

    java-lab-complited

--- src/main/java/com/example/java_task01/HelloController.java
+++ src/main/java/com/example/java_task01/HelloController.java
@@ -5,6 +5,9 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
 
 
 public class HelloController {
@@ -13,12 +16,20 @@ public class HelloController {
     public TextField rost;
     public Button btnRes;
     public Label msg;
-    public Slider imt;
+    public Circle circle;
+    public Rectangle progressBar;
 
     public void onCalcBMI(ActionEvent actionEvent) {
         bmi.setMass_p(mass.getText());
         bmi.setRost_p(rost.getText());
-        imt.setValue(bmi.getImt());
+        if (bmi.getImt() < 16) progressBar.setFill(Color.RED);
+        else if (bmi.getImt() < 18.5) progressBar.setFill(Color.rgb(255, 134, 134));
+        else if (bmi.getImt() < 25) progressBar.setFill(Color.rgb(255, 255, 255));
+        else if (bmi.getImt() < 30) progressBar.setFill(Color.rgb(255, 230, 230));
+        else if (bmi.getImt() < 35) progressBar.setFill(Color.rgb(246, 204, 204));
+        else if (bmi.getImt() < 40) progressBar.setFill(Color.rgb(255, 134, 134));
+        else progressBar.setFill(Color.RED);;
+        circle.setTranslateX(circle.getCenterX() + (bmi.getImt()*6));
         msg.setText(bmi.toString());
     }
 }
